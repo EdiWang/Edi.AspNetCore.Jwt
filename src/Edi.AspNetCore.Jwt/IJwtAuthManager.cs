950ba071c646db9a586b519d3b31d115f4d36c7d
Edi Wang <Edi.Wang@outlook.com>
2023-06-19 14:42:00 +0800

    add interface and type

---
+++ src/Edi.AspNetCore.Jwt/IJwtAuthManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Edi.AspNetCore.Jwt;
+
+public interface IJwtAuthManager
+{
+    public JwtTokenConfig JwtTokenConfig { get; }
+    IImmutableDictionary<string, RefreshToken> UsersRefreshTokensReadOnlyDictionary { get; }
+    JwtAuthResult GenerateTokens(string identifier, Claim[] claims, DateTime utcNow);
+    RefreshTokenResult Refresh(string refreshToken, string accessToken, string claimName, DateTime utcNow);
+    void RemoveExpiredRefreshTokens(DateTime utcNow);
+    void RemoveRefreshToken(string identifier);
+    (ClaimsPrincipal, JwtSecurityToken) DecodeJwtToken(string token);
+}
---
+++ src/Edi.AspNetCore.Jwt/RefreshTokenResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using System.Text.Json.Serialization;
+
+namespace Edi.AspNetCore.Jwt;
+
+public class RefreshTokenResult
+{
+    public string AccessToken { get; set; }
+
+    [JsonIgnore]
+    public RefreshToken RefreshToken { get; set; }
+
+    [JsonIgnore]
+    public ClaimsPrincipal ClaimsPrincipal { get; set; }
+}
