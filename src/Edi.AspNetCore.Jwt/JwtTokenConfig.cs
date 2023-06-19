808bef4d46030cf71c2b0064e4a88f3cdb6bf024
Edi Wang <Edi.Wang@outlook.com>
2023-06-19 13:49:31 +0800

    add types

---
+++ src/Edi.AspNetCore.Jwt/JwtTokenConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace Edi.AspNetCore.Jwt;
+
+public record JwtTokenConfig
+{
+    public string Secret { get; set; }
+
+    public string Issuer { get; set; }
+
+    public string Audience { get; set; }
+
+    public int AccessTokenExpiration { get; set; }
+
+    public int RefreshTokenExpiration { get; set; }
+}
---
+++ src/Edi.AspNetCore.Jwt/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace Edi.AspNetCore.Jwt;
+
+public record RefreshToken
+{
+    public string Identifier { get; set; }
+
+    public string TokenString { get; set; }
+
+    public DateTime ExpireAt { get; set; }
+}
